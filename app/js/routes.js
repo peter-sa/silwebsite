dcfe531807a7b40da347affa19b2b6da91cbe0ee
Peter Amidon <peter@picnicpark.org>
2014-05-23 10:36:44 -0700

    Add School Profile page and new routing mechanism
    This commit adds a new routing mechanism that does not require routes to
    be manually added for each page; instead, it attempts to serve all uris
    given to it and redirects to home if they are not available.  As a test
    of this routing mechanism, this commit also adds the beginnings of a
    school profile page.

--- app/js/routes.js
+++ app/js/routes.js
@@ -2,24 +2,47 @@
 
 angular.module('silwebsite.routes', ['ngRoute'])
 
+    // Since all of the page fetches are done in XHR requests, we
+    // can't serve a 404 back to the client through this, so let's
+    // just redirect to /home.
+    .config(['$httpProvider', '$provide', function($httpProvider, $provide) {
+        $provide.factory('404Interceptor', function($q, $injector) {
+            return {
+                'responseError': function(rejection, $location) {
+                    if (rejection.status == 404) {
+                        window.location="#/home";
+                        $location.path("/home");
+                        return $q.reject(rejection)
+                    } else {
+                        return $q.reject(rejection)
+                    }
+                }
+            }
+        });
+        $httpProvider.interceptors.push('404Interceptor');
+    }])
+
    // configure views; the authRequired parameter is used for specifying pages
    // which should only be available while logged in
-   .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when('/home', {
-         templateUrl: 'partials/home.html',
-         controller: 'HomeCtrl'
-      });
+    .config(['$routeProvider', function($routeProvider) {
 
-      $routeProvider.when('/account', {
-         authRequired: true, // must authenticate before viewing this page
-         templateUrl: 'partials/account.html',
-         controller: 'AccountCtrl'
-      });
+        $routeProvider.when('/:path*', {
+            //      templateUrl: function($routeParams) { return 'partials/'+$routeParams.path+'.html' },
+            templateUrl: function($routeParams) {
+                return 'partials/'+$routeParams.path+'.html'
+            },
+            controller: 'GenericCtrl'
+        })
+        $routeProvider.when('/account', {
+            authRequired: true, // must authenticate before viewing this page
+            templateUrl: 'partials/account.html',
+            controller: 'AccountCtrl'
+        });
 
-      $routeProvider.when('/login', {
-         templateUrl: 'partials/login.html',
-         controller: 'LoginCtrl'
-      });
+        $routeProvider.when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl'
+        });
 
-      $routeProvider.otherwise({redirectTo: '/home'});
-   }]);
+        $routeProvider.otherwise({redirectTo: '/home'});
+    }]);
